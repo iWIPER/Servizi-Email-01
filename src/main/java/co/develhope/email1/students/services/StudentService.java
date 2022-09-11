f092fbcf64c69957cf3d50f21e8dd1628fdbf7c3
Wilk Pereira <hhgchdbfgf@gmail.com>
2022-09-11 21:21:31 +0200

    Servizi - Email 01 - Exercise

---
+++ src/main/java/co/develhope/email1/students/services/StudentService.java
@@ -0,0 +1,28 @@
+package co.develhope.email1.students.services;
+
+import org.springframework.stereotype.Service;
+import co.develhope.email1.students.entities.Student;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    static List<Student> students = Arrays.asList(
+            new Student("1", "John", "Walker", "john@walker.com"),
+            new Student("2", "Walter", "White", "walter@white.com"),
+            new Student("3", "Samuel", "Dungeon", "samuel@dangeon.com"),
+            new Student("4", "Ursula", "Von Der Leyen", "ursula@boss.eu")
+            );
+
+    // Return the student
+    public Student getStudentById(String studentId) {
+        Optional<Student> studentFromList = students.stream().filter(student -> student.getId().equals
+                (studentId)).findAny();
+        if (studentFromList.isPresent())
+            return studentFromList.get();
+        return null;
+    }
+}
